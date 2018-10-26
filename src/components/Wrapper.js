34d42e438118c8eb5d8f9ad349e3c19c1b39158d
karim <beiranvand.karim@gmail.com>
2018-10-26 10:40:33 +0330

    sign in

---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -1,15 +1,60 @@
 import React, {Component, Fragment} from 'react';
-import {Link} from "react-router-dom";
+import {withRouter} from "react-router-dom";
+import Wrapper from "../components/Wrapper";
+import Input from "../components/Input";
 
 class SignIn extends Component {
+    constructor() {
+        super();
+        this.routeChange = this.routeChange.bind(this);
+    }
+    routeChange(){
+        let path = `dashBoard`;
+        this.props.history.push(path);
+    }
+
+    checkOnLogin = (e) => {
+        e.preventDefault();
+        let path = `dashBoard`;
+        if (this.state.userName === 'admin' && this.state.passWord === 'admin') {
+            this.props.history.push(path);
+        }
+    };
+
+    state = {
+        userName: '',
+        passWord: ''
+    };
+
+    change = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
     render() {
         return(
             <Fragment>
-                <h1>this is sign in page</h1>
-                <Link to="/dashBoard">dashBoard</Link>
+                <Wrapper>
+                    <form onSubmit={this.checkOnLogin.bind(this)}>
+                        <Input
+                            name="userName"
+                            onChange={e => this.change(e)}
+                            value={this.state.userName}
+                            placeholder="userName"
+                            type="text"/>
+                        <Input
+                            name="passWord"
+                            value={this.state.passWord}
+                            onChange={e => this.change(e)}
+                            placeholder="passWord"
+                            type="password"/>
+                        <button type="submit">sign in</button>
+                    </form>
+                </Wrapper>
             </Fragment>
         )
     }
 }
 
-export default SignIn;
+export default withRouter(SignIn);
